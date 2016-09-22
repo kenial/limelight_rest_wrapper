d007e61fc6637b717cba8b92804379bccf338e82
Kenial Lee <keniallee@gmail.com>
2016-09-22 16:01:28 -0700

    add POST request

--- README.md
+++ README.md
@@ -3,9 +3,9 @@ Wrapper of Limelight REST API (supports Python 3 only)
 
 Usage:
 ```
+# sample: Geographical reporting by cities in US (GET)
 ll = limelight_rest_wrapper.LimelightRESTWrapper("<limelight_id>", "<limelight_api_key>")
-# sample: Geographical reporting by cities in US
-endpoint = "https://control.llnw.com/geo-reporting-api/v1/countries/1/cities"    
+endpoint = "https://control.llnw.com/geo-reporting-api/v1/countries/1/cities"
 query_params = {
     "shortname": "test_service",    # service name - usually, your host name on limelight
     "service": "http",
@@ -13,5 +13,27 @@ query_params = {
     "startDate": "2016-08-07",  # if datetime format is needed, use isoformat() method of datetime
     "dataLimit": 50000,
 }
-ll.api_call(endpoint, query_params)
+ll.api_get(endpoint, query_params)
 ```
+
+```
+# sample: SmartPurge (POST)
+ll = limelight_rest_wrapper.LimelightRESTWrapper("<limelight_id>", "<limelight_api_key>")
+endpoint = "https://purge.llnw.com/purge/v1/account/{account}/requests"
+query_params = {
+    "patterns":[
+        {
+            "pattern":"http://test.site.com/temp/purge/*.jpg",
+            "evict":False,  # False: invalidate, True: evict (remove from cache entirely)
+            "exact":False,  # False: partial pattern, True: exact match
+            "incqs":False,  # False: on, True: include query string
+        },
+    ],
+    # "email":{
+    #     "subject":"purge results",
+    #     "to":"user@example.com"
+    # },
+    # "callback":{"url":"http://test.example.com/my_callback.php" },
+    "notes": "Test purge thru API",
+}
+res = ll.api_post(endpoint, query_params)
--- limelight_rest_wrapper.py
+++ limelight_rest_wrapper.py
@@ -3,6 +3,7 @@ import urllib
 import hmac
 import hashlib
 import requests
+import json
 
 class LimelightRESTWrapper(object):
     def __init__(self, limelight_user_id, api_key):
@@ -21,7 +22,7 @@ class LimelightRESTWrapper(object):
         token = hmac.new(bytes.fromhex(api_key), msg=datastring.encode("utf-8"), digestmod=hashlib.sha256).hexdigest()
         return token
 
-    def api_call(self, endpoint, query_params):
+    def api_get(self, endpoint, query_params):
         if endpoint == None or endpoint.strip() == "":
             raise Exception("REST API endpoint is needed")
         cur_timestamp = str(int(round(time.time()*1000)))
@@ -35,4 +36,20 @@ class LimelightRESTWrapper(object):
             'X-llnw-security-timestamp': cur_timestamp,
         }
         res = requests.get(endpoint, params=query_params, headers=headers)
-        return res.text
+        return res
+
+    def api_post(self, endpoint, post_data):
+        if endpoint == None or endpoint.strip() == "":
+            raise Exception("REST API endpoint is needed")
+        cur_timestamp = str(int(round(time.time()*1000)))
+        post_data_json = json.dumps(post_data, separators=(",",":"))
+        security_token = LimelightRESTWrapper.generate_security_token(url=endpoint, http_method="POST", post_data=post_data_json, api_key=self.api_key, timestamp=cur_timestamp)
+        headers = {
+            'Content-type': 'application/json',
+            'Accept': 'application/json',
+            'X-llnw-security-principal': self.llnw_security_principal,
+            'X-llnw-security-token': security_token,
+            'X-llnw-security-timestamp': cur_timestamp,
+        }
+        res = requests.post(endpoint, data=post_data_json, headers=headers)
+        return res
